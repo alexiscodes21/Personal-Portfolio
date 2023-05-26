d53237b202560880274c87878382b624333a420e
Alexis Winters <alexiscodes@yahoo.com>
2023-05-26 11:34:06 -0500

    change navbar to not use bootstrap and uninstalled bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
-// scroll sections active link
+// Toggle the navbar menu visibility
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+  var navbarMenu = document.getElementById("navbar-menu");
+  if (navbarMenu.style.display === "none") {
+    navbarMenu.style.display = "block";
+  } else {
+    navbarMenu.style.display = "none";
+  }
+});
+
+// Move the navbar menu items back to the original location in larger viewports
+window.addEventListener("resize", function () {
+  var navbarMenu = document.getElementById("navbar-menu");
+  var navbarToggle = document.getElementById("navbar-toggle");
+  if (window.innerWidth > 600) {
+    navbarMenu.style.display = "flex";
+    navbarToggle.style.display = "none";
+  } else {
+    navbarMenu.style.display = "none";
+    navbarToggle.style.display = "block";
+  }
+});
+
+// Initialize the display based on the viewport width
+window.addEventListener("load", function () {
+  var navbarMenu = document.getElementById("navbar-menu");
+  var navbarToggle = document.getElementById("navbar-toggle");
+  if (window.innerWidth > 600) {
+    navbarMenu.style.display = "flex";
+    navbarToggle.style.display = "none";
+  } else {
+    navbarMenu.style.display = "none";
+    navbarToggle.style.display = "block";
+  }
+});
+
+// Scroll sections active link 
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("nav a");
 
@@ -14,18 +50,18 @@ window.onscroll = () => {
         links.classList.remove("active");
         document.querySelector("nav a[href*=" + id + "]").classList.add("active");
       });
-    };
+    }
   });
 };
 
-// scroll reveal
+// Scroll reveal
 ScrollReveal({
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-container, .sectionHeading', { origin: 'top' });
-ScrollReveal().reveal('.skills-box, .projectCards, .contactSection form', { origin: 'bottom'});
-ScrollReveal().reveal('.home-container h1', { origin: 'left'});
+ScrollReveal().reveal(".home-container, .sectionHeading", { origin: "top" });
+ScrollReveal().reveal(".skills-container, .projects-container, .contactSection form", { origin: "bottom" });
+ScrollReveal().reveal(".home-container h1", { origin: "left" });
