e2644e5e46e508fdc97a56d1ec388755554720c3
Alexis Winters <alexiscodes@yahoo.com>
2023-06-07 07:06:08 -0500

    update email.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// import { keys } from "./keys.js";
-
 // Toggle the navbar menu visibility
 document.getElementById("navbar-toggle").addEventListener("click", function () {
   var navbarMenu = document.getElementById("navbar-menu");
@@ -36,37 +34,37 @@ window.addEventListener("load", function () {
   }
 });
 
-// EMAIL.JS
-// document.getElementById("form").addEventListener("submit", function (event) {
-//   event.preventDefault(); // Prevent form submission
-//   sendEmail();
-// });
+// email.js
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent form submission
+  sendEmail();
+});
 
-// function sendEmail() {
-//   // Get form values
-//   var name = document.getElementById("name").value;
-//   var email = document.getElementById("email").value;
-//   var subject = document.getElementById("subject").value;
-//   var message = document.getElementById("message").value;
+function sendEmail() {
+  // Get form values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
 
-//   // Prepare email parameters
-//   var templateParams = {
-//     from_name: name,
-//     from_email: email,
-//     from_subject: subject,
-//     to_name: "Alexis", // Recipient's name
-//     message: message,
-//   };
+  // Prepare email parameters
+  var templateParams = {
+    from_name: name,
+    from_email: email,
+    from_subject: subject,
+    to_name: "Alexis", // Recipient's name
+    message: message,
+  };
 
-//   // Send the email
-//   emailjs.init(keys.publicKey);
-//   emailjs.send(keys.serviceID, keys.templateID, templateParams).then(
-//     function (response) {
-//       console.log("Email sent! :)");
-//       alert("Your message has been sent! 👍🏽 I'll get back to you soon 😃")
-//     },
-//     function (error) {
-//       console.error("Error sending email :(");
-//     }
-//   );
-// }
+  // Send the email
+  emailjs.init("7ZFErJYAPNY5vE_w5");
+  emailjs.send("service_dggjnsd", "template_ptmtspe", templateParams).then(
+    function (response) {
+      console.log("Email sent! :)");
+      alert("Your message has been sent! 👍🏽 I'll get back to you soon 😃");
+    },
+    function (error) {
+      console.error("Error sending email :(");
+    }
+  );
+}
--- notes.md
+++ notes.md
@@ -1,5 +1,4 @@
 add these back when I fix email.js
 
-<script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js" type="text/javascript"></script>
 <script src="js/keys.js" type="module"></script>
 <script src="js/scripts.js" type="module"></script>
